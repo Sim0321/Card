5de902d1b2b9da4ee964b5a74632adb39259c0d0
Sim <0doogadooga@gmail.com>
2024-06-20 15:59:19 +0900

    전역 Color Palette 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { css } from "@emotion/react";
 import logo from "./logo.svg";
 import "./App.css";
+import styled from "@emotion/styled";
+
+const bold = css`
+  font-weigth: bold;
+`;
 
 const containerStyles = css`
   background-color: pink;
+  ${bold}
+`;
+
+const Button = styled.button`
+  width: 200px;
+  height: 100px;
 `;
 
 function App() {
   return (
     <div className="App" css={containerStyles}>
+      <Button>스타일 버튼</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
