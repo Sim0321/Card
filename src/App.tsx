4f1520fe4b37d076a3499cb66a9ccf1eb243357a
Sim <0doogadooga@gmail.com>
2024-07-24 17:30:38 +0900

    Text 공통 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
-import { css } from "@emotion/react";
-import logo from "./logo.svg";
 import "./App.css";
-import styled from "@emotion/styled";
-
-const bold = css`
-  font-weigth: bold;
-`;
-
-const containerStyles = css`
-  background-color: pink;
-  ${bold}
-`;
-
-const Button = styled.button`
-  width: 200px;
-  height: 100px;
-`;
+import Text from "@shared/Text";
 
 function App() {
   return (
-    <div className="App" css={containerStyles}>
-      <Button>스타일 버튼</Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t1" color="green">
+        t2
+      </Text>
+      <Text typography="t1" color="blue">
+        t3
+      </Text>
+      <Text typography="t1">t4</Text>
+      <Text typography="t1">t5</Text>
     </div>
   );
 }
