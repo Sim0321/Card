6a3e832cdf7d6849afd1522ec848ce80104fbd89
Sim <0doogadooga@gmail.com>
2024-07-31 16:09:42 +0900

    모든 정보 기입 완료, submit 함수 정의

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -1,25 +1,69 @@
 import Terms from '@components/apply/Terms';
 import CardInfo from '@components/apply/CardInfo';
 import BasicInfo from '@components/apply/BasicInfo';
-import { ApplyValues } from '@/models/apply';
+import { ApplyValues, APPLY_STATUS } from '@/models/apply';
+import { useEffect, useState } from 'react';
+import useUser from '@/hooks/auth/useUser';
+import { useParams } from 'react-router-dom';
 
-const Apply = ({ step, onSubmit }: { step: number; onSubmit: () => void }) => {
+const Apply = ({
+  onSubmit,
+}: {
+  onSubmit: (applyValues: ApplyValues) => void;
+}) => {
+  const user = useUser();
+  const { id } = useParams() as { id: string };
+
+  const [step, setStep] = useState(0);
+  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>({
+    userId: user?.uid,
+    cardId: id,
+  });
+
   // Pick으로 뽑아오면 객체로 뽑힘
   const handleTermsChange = (terms: ApplyValues['terms']) => {
-    console.log('terms ::', terms);
+    // console.log('terms ::', terms);
+    setApplyValues((prevValues) => ({
+      ...prevValues,
+      terms,
+    }));
+
+    setStep((prevStep) => prevStep + 1);
   };
 
   const handleBasicInfoChange = (
     infoValues: Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>,
   ) => {
-    console.log('infoValues ::', infoValues);
+    // console.log('infoValues ::', infoValues);
+    setApplyValues((prevValues) => ({
+      ...prevValues,
+      ...infoValues,
+    }));
+
+    setStep((prevStep) => prevStep + 1);
   };
 
   const handleCardInfoChange = (
     cardInfoValues: Pick<ApplyValues, 'isMaster' | 'isHipass' | 'isRf'>,
   ) => {
-    console.log('cardInfoValues ::', cardInfoValues);
+    // console.log('cardInfoValues ::', cardInfoValues);
+    setApplyValues((prevValues) => ({
+      ...prevValues,
+      ...cardInfoValues,
+    }));
+
+    setStep((prevStep) => prevStep + 1);
   };
+
+  useEffect(() => {
+    if (step === 3) {
+      onSubmit({
+        ...applyValues,
+        appliedAt: new Date(),
+        status: APPLY_STATUS.READY,
+      } as ApplyValues);
+    }
+  }, [applyValues, step, onSubmit]);
 
   return (
     <div>
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -3,13 +3,11 @@ import Terms from '@components/apply/Terms';
 import Apply from '@/components/apply';
 
 const ApplyPage = () => {
-  const [step, setStep] = useState(2);
-
   const handleSubmit = () => {
     // TODO 카드신청
   };
 
-  return <Apply step={step} onSubmit={handleSubmit} />;
+  return <Apply onSubmit={handleSubmit} />;
 };
 
 export default ApplyPage;
