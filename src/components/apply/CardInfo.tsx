4a854e0a8f5bf71a5e2b2d795b0151e298893ab8
Sim <0doogadooga@gmail.com>
2024-07-31 15:45:37 +0900

    카드 정보 완료

--- src/components/apply/CardInfo.tsx
+++ src/components/apply/CardInfo.tsx
@@ -2,37 +2,108 @@
 import { ApplyValues } from '@/models/apply';
 import Button from '@shared/Button';
 import Spacing from '@shared/Spacing';
-import { useState } from 'react';
+import { MouseEvent, useCallback, useState } from 'react';
+import FixedBottomButton from '../shared/FixedBottomButton';
 
 type CardInfoValues = Pick<ApplyValues, 'isMaster' | 'isHipass' | 'isRf'>;
 
-const CardInfo = () => {
-  const [] = useState<CardInfoValues>({
+const CardInfo = ({
+  onNext,
+}: {
+  onNext: (cardInfoValues: CardInfoValues) => void;
+}) => {
+  const [cardInfoValues, setCardInfoValues] = useState<CardInfoValues>({
     isMaster: false,
     isHipass: false,
     isRf: false,
   });
 
+  const { isMaster, isHipass, isRf } = cardInfoValues;
+
+  // onClick으로 cardInfoValues의 isMaster를 boolean 값으로 할 수도 있지만 비효율적이기 때문에 data set 사용
+  const handleButtonClick = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+    const $button = e.target as HTMLButtonElement;
+    // console.log($button.dataset.value);
+    const { name, dataset } = $button;
+
+    setCardInfoValues((prevValues) => ({
+      ...prevValues,
+      [name]: JSON.parse(dataset.value as string),
+    }));
+  }, []);
+
+  console.log(cardInfoValues);
+
   return (
     <div>
       <Button.Group title="해외결제">
-        <Button size="medium">Master</Button>
-        <Button size="medium">국내전용</Button>
+        <Button
+          name="isMaster"
+          weak={isMaster === false}
+          size="medium"
+          data-value={true}
+          onClick={handleButtonClick}
+        >
+          Master
+        </Button>
+        <Button
+          name="isMaster"
+          weak={isMaster === true}
+          size="medium"
+          data-value={false}
+          onClick={handleButtonClick}
+        >
+          국내전용
+        </Button>
       </Button.Group>
 
       <Spacing size={12} />
 
       <Button.Group title="후불교통기능">
-        <Button size="medium">신청안함</Button>
-        <Button size="medium">신청</Button>
+        <Button
+          name="isRf"
+          weak={isRf === true}
+          size="medium"
+          data-value={false}
+          onClick={handleButtonClick}
+        >
+          신청안함
+        </Button>
+        <Button
+          name="isRf"
+          weak={isRf === false}
+          size="medium"
+          data-value={true}
+          onClick={handleButtonClick}
+        >
+          신청
+        </Button>
       </Button.Group>
 
       <Spacing size={12} />
 
       <Button.Group title="후불하이패스카드">
-        <Button size="medium">신청안함</Button>
-        <Button size="medium">신청</Button>
+        <Button
+          name="isHipass"
+          weak={isHipass === true}
+          size="medium"
+          data-value={false}
+          onClick={handleButtonClick}
+        >
+          신청안함
+        </Button>
+        <Button
+          name="isHipass"
+          weak={isHipass === false}
+          size="medium"
+          data-value={true}
+          onClick={handleButtonClick}
+        >
+          신청
+        </Button>
       </Button.Group>
+
+      <FixedBottomButton label="다음" onClick={() => onNext(cardInfoValues)} />
     </div>
   );
 };
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -7,6 +7,8 @@ import { ApplyValues } from '@/models/apply';
 const Apply = () => {
   const [step, setStep] = useState(2);
 
+  // 현재 페이지가 할 작업은 카드 신청이지만 page 입장에선 데이터들이 궁금하지 않음. 그러므로 컴포넌트를 또 하나 생성해 데이터를 합친 최종 데이터만 받길 원함
+
   // Pick으로 뽑아오면 객체로 뽑힘
   const handleTermsChange = (terms: ApplyValues['terms']) => {
     console.log('terms ::', terms);
@@ -18,11 +20,17 @@ const Apply = () => {
     console.log('infoValues ::', infoValues);
   };
 
+  const handleCardInfoChange = (
+    cardInfoValues: Pick<ApplyValues, 'isMaster' | 'isHipass' | 'isRf'>,
+  ) => {
+    console.log('cardInfoValues ::', cardInfoValues);
+  };
+
   return (
     <div>
       {step === 0 && <Terms onNext={handleTermsChange} />}
       {step === 1 && <BasicInfo onNext={handleBasicInfoChange} />}
-      {step === 2 && <CardInfo />}
+      {step === 2 && <CardInfo onNext={handleCardInfoChange} />}
     </div>
   );
 };
